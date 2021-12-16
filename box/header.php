8d07005ea358c64309d9102f72af98cd87ceb12b
01Junel <52649280+01Junel@users.noreply.github.com>
2021-12-16 14:51:30 +0800

    Create header.php

---
+++ box/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html> 
+<html lang="en-US">
+    <head>
+        <!--Meta Tags Starts Here-->
+        <meta charset="UTF-8" />
+        <meta name="author" content="STUDENT ASSESSMENT" /> 
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+        <!--Meta Tags Ends Here-->
+        <title>STUDENT ASSESSMENT</title>
+        
+        <!--COUNT DOWN TIMER STARTS HERE-->
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.js"></script>
+        <script src="<?php echo SITEURL; ?>/assets/js/countdown/jquery.simple.timer.js"></script>
+        <script>
+          $(function(){
+            $('.timer').startTimer();
+          });
+        </script>
+        <!--COUNT DOWN TIMER ENDS HERE-->
+        
+        <!--ADDING CKEDITOR HERE-->
+        <script type="text/javascript" src="<?php echo SITEURL; ?>/assets/ckeditor/ckeditor.js"></script>
+        
+        <!--CSS File Starts Here-->
+        <!-- <link rel="stylesheet" type="text/css" href="<?php echo SITEURL; ?>/assets/css/style.css" /> -->
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+        <!--CSS File Ends Here-->
+    </head>
