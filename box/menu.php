cb477eb5f5cab583d9b9e226bd39fb16de43e035
01Junel <52649280+01Junel@users.noreply.github.com>
2021-12-16 14:53:52 +0800

    Create menu.php

---
+++ box/menu.php
@@ -0,0 +1,23 @@
+<!--Navigation Starts Here-->
+        <nav class="menu">
+            <div class="wrapper">
+                
+                <ul>
+                    <a href="welcome.php"><li>Home</li></a>
+                    <a href="developers.php"><li>Developers</li></a>
+                    <a href="contacts.php"><li>Contacts</li></a>
+                  <!--  <a href="student.php"><li>Students</li></a>
+                    <a href="question.php"><li>Questions</li></a>
+                    <a href="result.php"><li>Result</li></a> -->
+                    
+                    
+                    <!--<a href="<?php echo SITEURL; ?>admin/index.php?page=students"><li>Students</li></a>
+                    <a href="<?php echo SITEURL; ?>admin/index.php?page=faculties"><li>Faculties</li></a>
+                    <a href="<?php echo SITEURL; ?>admin/index.php?page=questions"><li>Questions</li></a>
+                    <a href="<?php echo SITEURL; ?>admin/index.php?page=results"><li>Results</li></a>
+                    <a href="<?php echo SITEURL; ?>admin/index.php?page=settings"><li>Settings</li></a>
+                    <a href="../index.php" onclick="return confirm('Are you sure?')"><li>Log Out</li></a> -->
+                </ul>
+            </div>
+        </nav>
+        <!--Navigation Ends Here-->
