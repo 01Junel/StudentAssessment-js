b02e250808bb1cd41aece8cb251b64ee63ce56da
01Junel <52649280+01Junel@users.noreply.github.com>
2021-12-16 14:52:38 +0800

    Create footer.php

---
+++ box/footer.php
@@ -0,0 +1,11 @@
+<!--Footer Starts Here-->
+        <footer class="footer">
+            <div class="wrapper">
+                <p>
+                    All Rights Reserved.    
+                    By The Developer.
+                </p>
+            </div>
+        </footer>
+        <!--Footer Ends Here-->
+</html>
